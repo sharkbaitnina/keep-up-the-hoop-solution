3891b552bba79476cf094cb3c1f7a142a5a1465a
Nina Valenzuela <98240042+sharkbaitnina@users.noreply.github.com>
2022-10-05 08:41:09 -0700

    Create hoop-solution.js

---
+++ hoop-solution.js
@@ -0,0 +1,9 @@
+function hoopCount (n) {
+  //pay attention to how the signs are facing 
+  if (n >= 10){
+    //got an error because I had n=> 10 instead
+    return("Great now move on to tricks")
+  }else{
+    return("Keep at it until you get it")
+  }
+}
